491ee69e59a594c02904eef939074b720a19fcb1
Ben Fox <benfox1216@gmail.com>
2020-02-18 16:24:29 -0700

    Set test up for registration page

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "When I visit the registration page" do
+    before :each do
+      
+    end
+  
+    it "I can create a new profile" do
+      
+    end
+  end
+end
