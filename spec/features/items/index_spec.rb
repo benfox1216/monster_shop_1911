01dca5e2a0504327c0f11464a3246d050a1f9152
Ben Fox <benfox1216@gmail.com>
2020-02-20 14:20:37 -0700

    Finish story 17

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "Items Index Page" do
       @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
     end
 
-    it "all items or merchant names are links" do
+    it "all items, merchant names, and images are links" do
       visit '/items'
 
       expect(page).to have_link(@tire.name)
@@ -20,6 +20,9 @@ RSpec.describe "Items Index Page" do
       expect(page).to have_link(@pull_toy.name)
       expect(page).to have_link(@pull_toy.merchant.name)
       expect(page).to_not have_link(@dog_bone.name)
+      
+      click_link("img-#{@tire.id}")
+      expect(current_path).to eq("/items/#{@tire.id}")
     end
 
     it "I can see a list of all of the items, except disabled items" do
