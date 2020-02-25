dd1e8a8e39f98d31939dc586ffb2f97a7e12e8a2
Ezekiel Clark <clarkzeke@gmail.com>
2020-02-25 10:28:53 -0700

    Add item show page from merchant dashboard

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,5 +1,6 @@
 class Merchant::ItemsController < Merchant::BaseController
   def show
     @merchant = current_user.merchant
+    @items = Item.where("items.merchant_id = #{@merchant.id}")
   end
 end
